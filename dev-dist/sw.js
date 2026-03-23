e02d647d5c67e2e4bbded6de505f11395c5cb906
nrsyzwni <syazwanimsubri@gmail.com>
2026-03-23 13:22:13 +0800

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.9h2um4kdcpg"
+    "revision": "0.no8c92v96v"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
